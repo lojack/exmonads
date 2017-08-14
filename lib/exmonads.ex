73a5b9f019865fd7d6b504a40f1ba8ade01cf4e5
Robert Clark <rclark@covermymeds.com>
2017-08-13 23:12:04 -0400

    removed examples

--- lib/exmonads.ex
+++ lib/exmonads.ex
@@ -1,8 +1,10 @@
 defmodule Exmonads do
   defmacro {:error, message} ~> _, do: {:error, message}
+
   defmacro {:ok, input} ~> {method, meta, args} do
     {method, meta, [input | args]}
   end
+
   defmacro left ~> right do
     quote do
       case unquote(left) do
@@ -10,19 +12,5 @@ defmodule Exmonads do
         {:ok, value} -> {:ok, value} ~> unquote(right)
       end
     end
-  end
-end
-
-defmodule Exmonads.Examples do
-  def divide_by(input, divisor) do
-    try do
-      {:ok, input / divisor}
-    rescue
-      _e in ArithmeticError -> {:error, "Divide by zero"}
-    end
-  end
-
-  def add_by(input, more) do
-    {:ok, input + more}
   end
 end
